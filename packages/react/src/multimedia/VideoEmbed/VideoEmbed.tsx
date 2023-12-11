ae01494fbce541aa3e28b067da4cedf2480ef84e
Romu-C <romuald.coulaud@gmail.com>
2023-12-11 13:50:34 +0100

    feat: #WB-2012 Update with PR feedback

--- packages/react/src/multimedia/VideoEmbed/VideoEmbed.tsx
+++ packages/react/src/multimedia/VideoEmbed/VideoEmbed.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 
-import { ArrowRight, Globe, ImageNotFound } from "@edifice-ui/icons";
+import { ArrowRight, Globe } from "@edifice-ui/icons";
 import { Embedder, odeServices } from "edifice-ts-client";
 import { useTranslation } from "react-i18next";
 
-import { Button, FormControl, Image, Input } from "../../components";
+import {
+  Button,
+  EmptyScreen,
+  FormControl,
+  Image,
+  Input,
+} from "../../components";
+import { usePaths } from "../../core";
 import { useDebounce } from "../../hooks";
 import { useMediaLibraryContext } from "../MediaLibrary/MediaLibraryContext";
 
@@ -20,6 +27,8 @@ const VideoEmbed = ({ onSuccess }: VideoEmbedProps) => {
   const debounceChangeUrl = useDebounce<string>(url || "", 300);
   const [whiteListProvider, setWhiteListProvider] = useState<Embedder[]>();
   const { switchType } = useMediaLibraryContext();
+
+  const [imagePath] = usePaths();
 
   useEffect(() => {
     initWhiteListProvider();
@@ -67,8 +76,68 @@ const VideoEmbed = ({ onSuccess }: VideoEmbedProps) => {
     switchType("embedder");
   }
 
+  const renderContent = () => {
+    if (debounceChangeUrl) {
+      if (embedder) {
+        return (
+          <div className="mx-auto mt-16">
+            <div className="video-embed-provider  d-flex align-items-center">
+              <Image
+                src={embedder.logo}
+                alt={"Logo " + embedder.displayName}
+                className="video-embed-provider__logo"
+              ></Image>
+              {embedder.displayName}
+            </div>
+            {embedVideo && (
+              <div
+                className="video-embed-preview mt-12"
+                dangerouslySetInnerHTML={{
+                  __html: embedVideo,
+                }}
+              ></div>
+            )}
+          </div>
+        );
+      } else {
+        return (
+          <div className="d-flex flex-column align-items-center m-16">
+            <EmptyScreen
+              imageSrc={`${imagePath}/emptyscreen/illu-error.svg`}
+              title={t("Prévisualisation de la vidéo non disponible")}
+              text={t(
+                "Le lien n'est pas reconnu, vous pouvez utiliser un code embed ou iframe, afin qu'elle s'affiche correctement.",
+              )}
+            />
+            <Button
+              variant="ghost"
+              color="secondary"
+              onClick={handleSwitchToEmbedderClick}
+              className="align-items-start mt-16"
+            >
+              {t("Utiliser un code embed ou iframe")} <ArrowRight />
+            </Button>
+          </div>
+        );
+      }
+    } else {
+      return (
+        <div className="d-flex my-16 align-items-start">
+          <Button
+            variant="ghost"
+            color="secondary"
+            onClick={handleSwitchToEmbedderClick}
+            className="align-items-start"
+          >
+            {t("Utiliser un code embed ou iframe")} <ArrowRight />
+          </Button>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="d-flex flex-column flex-fill">
+    <div className="d-flex flex-column flex-fill video-embed">
       <div className="mb-8 d-flex">
         <Globe className="me-8"></Globe>
         {t("URL de la vidéo")}
@@ -81,59 +150,7 @@ const VideoEmbed = ({ onSuccess }: VideoEmbedProps) => {
           onChange={handleUrlChange}
         />
       </FormControl>
-      {debounceChangeUrl && embedder && (
-        <div className="mx-auto mt-16">
-          <div className="video-embed-provider  d-flex align-items-center">
-            <Image
-              src={embedder.logo}
-              alt={"Logo " + embedder.displayName}
-              className="video-embed-provider__logo"
-            ></Image>
-            {embedder.displayName}
-          </div>
-          {embedVideo && (
-            <div
-              className="video-embed-preview mt-12"
-              dangerouslySetInnerHTML={{
-                __html: embedVideo,
-              }}
-            ></div>
-          )}
-        </div>
-      )}
-      {debounceChangeUrl && !embedder && (
-        <div className="d-flex flex-column align-items-center m-16">
-          <ImageNotFound height="150px" width="150px" />
-          <div className="h4 mt-16">
-            {t("Prévisualisation de la vidéo non disponible")}
-          </div>
-          <div className="mb-16">
-            {t(
-              "Le lien n'est pas reconnu, vous pouvez utiliser un code embed ou iframe, afin qu'elle s'affiche correctement.",
-            )}
-          </div>
-          <Button
-            variant="ghost"
-            color="secondary"
-            onClick={handleSwitchToEmbedderClick}
-            className="align-items-start"
-          >
-            {t("Utiliser un code embed ou iframe")} <ArrowRight />
-          </Button>
-        </div>
-      )}
-      {!debounceChangeUrl && (
-        <div className="d-flex my-16 align-items-start">
-          <Button
-            variant="ghost"
-            color="secondary"
-            onClick={handleSwitchToEmbedderClick}
-            className="align-items-start"
-          >
-            {t("Utiliser un code embed ou iframe")} <ArrowRight />
-          </Button>
-        </div>
-      )}
+      <>{renderContent()}</>
     </div>
   );
 };
